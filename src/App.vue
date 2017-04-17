dd8a9c410f8626d850bbba4d7fd1dee3d131ed26
Humeira Diljore <hello@echdee.me>
2017-04-17 14:04:00 +0400

    styling

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,40 @@
 <template>
     <div id="app">
-        <timer date-props="December 15, 2017"></timer>
+        <div class="container">
+            <div class="container-gradient">
+                <timer date-props="December 15, 2017"></timer>
+            </div>
+
+        </div>
     </div>
 </template>
+<style>
+    body {
+        margin: 0;
+        padding: 0
+    }
 
+    .container {
+        width: 100%;
+        background: url('./assets/img/poster.png') center center no-repeat;
+        /*background-size: 75%;*/
+        background-color: #190206;
+        height: 100vh;
+        min-height: 100%;
+        position: relative;
+        overflow: hidden;
+    }
+
+    .container-gradient {
+        z-index: 0;
+        width: 100%;
+        height: 100%;
+        position: absolute;
+        opacity: 0.8;
+        background: -webkit-linear-gradient(126deg, #190206 50%, #8b0000 50%);
+        background: linear-gradient(126deg, #190206 50%, #8b0000 50%);
+    }
+</style>
 <script>
     import Timer from './components/timer.vue';
     export default {
